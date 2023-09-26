0bf9f2b3d8563673929ee0c0780a094c7c008196
ascetik <ascetik@free.fr>
2023-09-26 20:14:19 +0200

    new KronoState interface
    to use a state machine to handle krono

--- src/Krono.php
+++ src/Krono.php
@@ -28,7 +28,7 @@ class Krono
             throw new KronoException('not started');
         }
         $this->stop = hrtime();
-        // $this->isRunning = false;
+        $this->isRunning = false;
         return $this->stop;
     }
 
@@ -43,4 +43,21 @@ class Krono
         $unit = TimeScaler::unit($time);
         return (string) $unit;
     }
+    /**
+     * J'ai un chrono pas encore lancé.
+     * je ne peux pas stopper ni afficher de résultat, ni cancel.
+     * Je peux faire reset mais ça ne sert à rien, j'y suis déjà.
+     * InitialState
+     * Je le lance avec start()
+     * à ce moment là, je ne dois pouvoir faire que stop(), restart() et cancel()
+     * je ne peux pas demander de sortie
+     * RunningState
+     * je ne peux restart(), stop(), cancel() qu'en RunningState
+     * Si j'ai fait restart, je réinitialise mes valeurs et je rappelle start()
+     * si j'ai fait cancel(), je reprends l'état du début
+     * InitialState
+     * ensuite je fais stop(). là je peux faire reset() ou obtenir mon résultat, float ou string. Je ne peux pas cancel()
+     * ReadyState
+     *
+     */
 }
---
+++ src/Types/KronoState.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Ascetik\Krono\Types;
+
+interface KronoState
+{
+    //
+}
