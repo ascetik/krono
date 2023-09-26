409f181f813bb6b3ee1a4f710e5c0549f1e81e2f
ascetik <ascetik@free.fr>
2023-09-26 21:05:59 +0200

    new Counter interface

---
+++ src/Types/Counter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ascetik\Krono\Types;
+
+interface Counter
+{
+    public function start(): float;
+    public function stop(): float;
+    public function reset(): float;
+    public function cancel(): void;
+}
